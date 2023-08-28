345359f3c98aabf26bb9963458fae58d013b0088
Jaehoon Park <wido1593@gmail.com>
2023-08-28 20:42:12 +0900

    1. 3챕터 JPA 파트 수정.

--- src/main/java/com/skateboard/data/PartsRepository.java
+++ src/main/java/com/skateboard/data/PartsRepository.java
@@ -1,13 +1,8 @@
 package com.skateboard.data;
 
 import com.skateboard.DTO.Parts;
+import org.springframework.data.repository.CrudRepository;
 
-public interface PartsRepository {
-    //Iterable 은 오브젝트를 for 문이나 for-loop와 같이 다수의 결과를 호출 할 수 있게하는 타입이다.
-    Iterable<Parts> findAll();
+public interface PartsRepository extends CrudRepository<Parts, String> {
 
-    //인자로 id를 함께 넘겨 where 절의 value로 제공하기 위해.
-    Parts findById(String id);
-
-    Parts save(Parts parts);
 }
